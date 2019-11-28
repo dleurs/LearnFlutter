0bf52691f80b1e46c680cbbee1f1842a0030c96a
dleurs <dimitri.leurs@gmail.com>
2019-11-28 13:51:49 +0100

    Stateful Widget implemented

--- 2AppFromScratch/app_from_scratch/lib/main.dart
+++ 2AppFromScratch/app_from_scratch/lib/main.dart
@@ -4,8 +4,45 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _indexQuestion = 0;
+
+  void _clickButtonQuestion(int inputIndexQuestion) {
+    setState(() {
+      _indexQuestion = inputIndexQuestion;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text("Hello !"));
+    var questions = [
+      "What is your favorite sport ?",
+      "What is your favorite game ?",
+      "What is your favourite programming language ?"
+    ];
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(title: Text('My First App')),
+          body: Column(children: [
+            Text(questions[_indexQuestion]),
+            RaisedButton(
+              child: Text('Question 1'),
+              onPressed: () => _clickButtonQuestion(0),
+            ),
+            RaisedButton(
+              child: Text('Question 2'),
+              onPressed: () => _clickButtonQuestion(1),
+            ),
+            RaisedButton(
+              child: Text('Question 3'),
+              onPressed: () => _clickButtonQuestion(2),
+            ),
+          ])),
+    );
   }
 }
