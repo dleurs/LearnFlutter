eef86c96c0c962a79b29f115954190816c257f10
dleurs <dimitri.leurs@gmail.com>
2019-12-16 00:02:57 +0100

    test

---
+++ 5CourseUdemy/udemy_course/lib/quiz.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import './answer.dart';
+import './question.dart';
+
+class Quiz extends StatelessWidget {
+  final List<Map<String, Object>> questionsAndAnswers;
+  final int indexQuestion;
+  final Function clickButtonQuestion;
+
+  Quiz(
+      {this.questionsAndAnswers,
+      this.indexQuestion,
+      this.clickButtonQuestion});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Question(questionsAndAnswers[indexQuestion]['quesionText']),
+      ...(questionsAndAnswers[indexQuestion]["answers"] as List<Map<String, Object>>)
+          .map((answer) {
+        //Answer(questionText: answer, toExecuteOnPressed: _clickButtonQuestion),
+        return Answer(
+            questionText: answer["text"], toExecuteOnPressed: () => clickButtonQuestion(answer["score"]));
+      }).toList()
+    ]);
+  }
+}
