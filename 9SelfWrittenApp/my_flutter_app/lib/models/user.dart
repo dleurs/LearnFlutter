812e748a48ef5c8f53968577ae457f4a5dc657db
dleurs <dimitri.leurs@gmail.com>
2019-12-19 21:50:15 +0100

    project 9 : User object

---
+++ 9SelfWrittenApp/my_flutter_app/lib/models/user.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/foundation.dart';
+
+class User {
+
+  final String uid;
+  bool _isAnonymous;
+  String _username;
+  String _email;
+
+  User({@required this.uid, String username, String email }) {
+    this._isAnonymous = false;
+    this._username = username;
+    this._email = email;
+  }
+  
+  User.anonymous({@required this.uid}) {
+    this._isAnonymous = true;
+    this._username = null;
+    this._email = null;
+  }
+  
+  bool getIsAnonymous() => _isAnonymous;
+
+}
